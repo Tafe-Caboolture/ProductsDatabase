cb3e9d64bf37c2bc0f043c3a2e0bf5853fda3bac
Matt-The-Programmer <mattmilzewski@gmail.com>
2018-02-13 09:40:08 +1000

    Add project files.

---
+++ ProductsDatabase/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProductsDatabase.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Products</h1>
+           <%-- These C# comments will be stripped out by the server at runtime --%>
+            <%-- This HTML comment will still be sent to the client --%>
+            <%-- ID Name Price --%>
+            ID: <asp:Label runat="server" ID="lblID">0</asp:Label> <br />
+            Name: <asp:TextBox runat="server" ID="txtName" /> <br />
+           <%-- The textmode attribute maps to the attribute of an <input> element
+               compatability depends on your browser--%>
+            Price: <asp:TextBox runat="server" ID="txtPrice" /> <br />
+            <asp:Button runat="server" ID="btnFirst" Text="First" OnClick="btnFirst_Click" />
+             <asp:Button runat="server" ID="btnPrevious" Text="Previous" OnClick="btnPrevious_Click" />
+            <asp:Button runat="server" ID="btnLast" Text="Last" OnClick="btnLast_Click" />
+            <asp:Button runat="server" ID="btnNext" Text="Next" OnClick="btnNext_Click" />
+           
+            <%-- line break between navigation and data buttons --%>
+            <br />
+            <asp:Button runat="server" ID="btnNew" Text="New" OnClick="btnNew_Click" />
+            <asp:Button runat="server" ID="btnSave" Text="Save" OnClick="btnSave_Click" />
+            <asp:Button runat="server" ID="btnDelete" Text="Delete" OnClick="btnDelete_Click" />
+
+
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProductsDatabase/Default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+namespace ProductsDatabase
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        // don't add any instance variables either
+        //they wont work the way you expect them to
+        //will be explained in a coming lesson
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // don't put anything in here for todays lesson
+        }
+
+        protected void btnFirst_Click(object sender, EventArgs e)
+        {
+            // get file path of database using server side functions
+            var dbFile = Server.MapPath("~/App_Data/Products.accdb");
+            //server map Path accepts relative paths
+            //it will return where the path specified is on the server
+            // with the ~ representing the root of our web site
+            var constring = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={dbFile}";
+            var query = "SELECT TOP 1 * FROM tblProducts";
+            // using statements like this one will automatically dispose of objects
+            // that are created (such as our db object
+            //objects that implement the Disposable interface may have special
+            //behaviour for when they are disposed for example
+            // ADO.NET connection objects will usually disconnect when they're done
+            using (var db = new OleDbConnection(constring))
+            {
+                db.Open();
+
+                // we dont really need the command object
+                // so we can combine the using statement with the method chaining
+                //to only get a useful reader object
+                using (var reader = new OleDbCommand(query, db).ExecuteReader())
+                {
+                    reader.Read();
+                    lblID.Text = reader["ProductID"].ToString();
+                    txtName.Text = reader["ProductName"].ToString();
+                    txtPrice.Text = reader["ProductPrice"].ToString();
+                }
+            }
+        }
+
+        protected void btnPrevious_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLast_Click(object sender, EventArgs e)
+        {
+            // get file path of database using server side functions
+            var dbFile = Server.MapPath("~/App_Data/Products.accdb");
+            //server map Path accepts relative paths
+            //it will return where the path specified is on the server
+            // with the ~ representing the root of our web site
+            var constring = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={dbFile}";
+            var query = "SELECT * FROM tblProducts ORDER BY ProductID DESC";
+            // using statements like this one will automatically dispose of objects
+            // that are created (such as our db object
+            //objects that implement the Disposable interface may have special
+            //behaviour for when they are disposed for example
+            // ADO.NET connection objects will usually disconnect when they're done
+            using (var db = new OleDbConnection(constring))
+            {
+                db.Open();
+
+                // we dont really need the command object
+                // so we can combine the using statement with the method chaining
+                //to only get a useful reader object
+                using (var reader = new OleDbCommand(query, db).ExecuteReader())
+                {
+                    reader.Read();
+                    lblID.Text = reader["ProductID"].ToString();
+                    txtName.Text = reader["ProductName"].ToString();
+                    txtPrice.Text = reader["ProductPrice"].ToString();
+                }
+            }
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
